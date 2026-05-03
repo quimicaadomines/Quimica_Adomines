33236d6de03065eaa939c61decc627da8b7f7ae5
Zayde Lucas de Sousa Castro <zaidlucass1909@gmail.com>
2026-05-02 21:57:21 -0300

    Update assistente.js

--- api/assistente.js
+++ api/assistente.js
@@ -17,28 +17,32 @@ Você DEVE obrigatoriamente responder usando APENAS o JSON, sem nenhum texto adi
 
 AÇÕES PERMITIDAS PARA O CAMPO "acao":
 - "FECHAR_CONFIG", "FECHAR_TABELA", "FECHAR_CONQUISTAS", "FECHAR_CHAT", "FECHAR_TUDO"
-- "ABRIR_CONFIG", "ABRIR_TABELA", "ABRIR_CONQUISTAS", "ABRIR_CHAT", "ABRIR_TUTORIAL", "IR_TUTORIAL"
+- "ABRIR_CONFIG", "ABRIR_TABELA", "ABRIR_CONQUISTAS", "ABRIR_CHAT", "ABRIR_TUTORIAL", "IR_TUTORIAL", "ABRIR_CATALOGO"
 - "DIMINUIR_MUSICA", "DIMINUIR_EFEITOS", "DESLIGAR_VISUAIS", "LIGAR_VISUAIS"
 - "TEMA_CLARO", "TEMA_ESCURO", "MUTAR_SOM", "DESMUTAR_SOM"
-- "LER_TELA", "LER_ENUNCIADO", "LER_ALTERNATIVAS", "LER_TUTORIAL"
+- "LER_TELA", "LER_ENUNCIADO", "LER_ALTERNATIVAS", "LER_TUTORIAL", "LER_CATALOGO", "LER_CONQUISTAS", "LER_ATOMOS_DISPONIVEIS"
 - "STATUS_VIDAS", "STATUS_ESTRELAS", "STATUS_TEMPO"
 - "IR_MODOS", "VOLTAR"
 - "JOGAR_ESTRUTURANDO" (detalhe: "livre", "facil", "medio", "dificil", "impossivel")
 - "JOGAR_INCLUSIVO" (detalhe: "reconhecer", "relacionar", "interpretar")
 
+Ações Avançadas e Chat:
+- "COMANDO_ADM" (Para atalhos de chat. detalhe: "\\platinar", "\\catalogador", "\\limpar" ou "\\completar")
+- "CONSULTAR_TABELA_VOZ" (Se o jogador perguntar dados da tabela periódica. detalhe: "NomeDoAtomo|tipoDeInformacao". Ex: "Carbono|ligacoes", "Oxigenio|massa", "Fluor|numero")
+
 Ações de Quadro Estruturando (Montagem) e Ferramentas:
 - "CRIAR_ATOMO" (detalhe: Ex: "Carbono")
 - "CRIAR_LIGACAO" (detalhe: "simples", "dupla" ou "tripla")
 - "ADICIONAR_LIGACAO_ATOMO" (detalhe: "Nome do Átomo|tipo|direção". Ex: "Carbono 1|simples|esquerda" ou "Oxigênio 2|dupla|cima". Direções válidas: esquerda, direita, cima, baixo)
-- "LIGAR_ATOMOS" (detalhe: "Átomo A|Átomo B|Ligacao". Ex: "Carbono 1|Carbono 2|simples")
+- "LIGAR_ATOMOS" (MUITO IMPORTANTE: Use este comando se o jogador pedir para ligar dois átomos existentes OU se ele pedir para "adicionar um átomo ligado em outro". detalhe: "Átomo A|Átomo B|Ligacao". Ex: "Carbono 1|Oxigênio 1|simples")
 - "COMPLETAR_VALENCIA", "DESVINCULAR_PECA", "EXCLUIR_PECA" (detalhe: Ex: "Carbono 1")
 - "LIMPAR_QUADRO", "LER_QUADRO", "VERIFICAR_ESTRUTURA", "CONFIRMAR_CLASSIFICACAO"
-- "DICA_DESAFIO", "TIRAR_FOTO", "DESFAZER_ACAO", "GIRAR_MOLECULAS"
-- "ZOOM_MAIS", "ZOOM_MENOS", "ZOOM_RESET", "ABRIR_CATALOGO", "INFO_INCLUSIVA", "CONCLUIR_PINTURA"
+- "DICA_DESAFIO", "TIRAR_FOTO", "DESFAZER_ACAO", "GIRAR_MOLECULAS", "ZOOM_MAIS", "ZOOM_MENOS", "ZOOM_RESET"
+- "INFO_INCLUSIVA", "CONCLUIR_PINTURA"
 - "COR_BORRACHA", "COR_VERMELHA", "COR_AZUL", "COR_AMARELA", "COR_PRETA", "COR_VERDE", "COR_CINZA"
 
 EXEMPLO DE RESPOSTA (Sempre neste formato):
-{"acao": "ADICIONAR_LIGACAO_ATOMO", "detalhe": "Carbono 1|simples|esquerda"}
+{"acao": "LIGAR_ATOMOS", "detalhe": "Carbono 1|Oxigênio 1|simples"}
     `;
 
     try {
